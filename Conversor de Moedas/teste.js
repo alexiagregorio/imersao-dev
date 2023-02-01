36a4ef0b7d66afbaa72e96f81441a9372fbf9651
Alexia Gregorio Dias <alexiagregorio6@hotmail.com>
2023-02-01 16:33:01 -0300

    att conversor

--- Conversor de Moedas/teste.js
+++ Conversor de Moedas/teste.js
@@ -1,6 +1,15 @@
-var valorEmDolar = 30;
+/* var valorEmDolar = 30;
 var cotacaoDoDolar = 5.32;
 
 var valorEmReal = valorEmDolar*cotacaoDoDolar;
 valorEmReal = valorEmReal.toFixed(2);
 alert(`R$${valorEmReal}`);
+
+*/
+
+var valorEmReal = prompt('Digite um valor em real para converter em dólar.');
+var cotacaoDoReal = 0.20;
+
+var valorEmDolar = valorEmReal * cotacaoDoReal;
+valorEmDolar = valorEmDolar.toFixed(2);
+alert(`R$${valorEmReal} valem US$${valorEmDolar}`)
